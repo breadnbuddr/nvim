3b3892ffb4b964a584b73e5a2a89c43a3f481cff
breadnbuddr <117009437+breadnbuddr@users.noreply.github.com>
2024-12-23 21:43:38 +0100

    Configure core setup - Add comments

--- lua/bread/core/lazy.lua
+++ lua/bread/core/lazy.lua
@@ -1,3 +1,5 @@
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -15,6 +17,18 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
