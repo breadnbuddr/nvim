6f02dc521ab6ac01a8010e12d6cc0d8cc44bb513
breadnbuddr <117009437+breadnbuddr@users.noreply.github.com>
2024-12-23 21:38:17 +0100

    Configure core setup - Add initial setup for lazy plugin manager (will throw an 'error' as there are no plugins yet)

--- lua/bread/core/keymaps.lua
+++ lua/bread/core/keymaps.lua
@@ -8,6 +8,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Set leader key to space
 vim.g.mapleader = " "
 
+-- Set local leader key to backslash
+vim.g.maplocalleader = "\\"
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
